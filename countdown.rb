e0097f7eb31787badd02ebfe88e743e58e7017d5
Don Ward <10is0420@gmail.com>
2019-06-29 14:56:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+def countdown(time)
+  while time >= 0 
+    puts "#{time} SECOND(S)!"
+    time -= 1
+  end
+  puts 'HAPPY NEW YEAR!'
+end
 
-def countdown
+def countdown_with_sleep(time)
+  
 end
